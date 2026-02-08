cef5b9f8a85f6640d95720ce1fec9837d7c3a06c
Zin Min Htet <zinmin337890@gmail.com>
2026-02-08 21:46:47 +0630

    Feat-implement-course-structure-management-with-chapter-and-lesson-support-2026-2-8

---
+++ app/data/admin/admin-get-course.ts
@@ -0,0 +1,52 @@
+import "server-only"
+import { requireAdmin } from "./require-admin"
+import { prisma } from "@/lib/db";
+import { notFound } from "next/navigation";
+
+export async function adminGetCourse(id: string) {
+    await requireAdmin();
+
+    const data = await prisma.course.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            fileKey: true,
+            price: true,
+            duration: true,
+            level: true,
+            status: true,
+            smallDescription: true,
+            category: true,
+            slug: true,
+            chapter: {
+                select: {
+                    id: true,
+                    title: true,
+                    position: true,
+                    lessons: {
+                        select: {
+                            id: true,
+                            title: true,
+                            description: true,
+                            thumbnailKey: true,
+                            position: true,
+                            videoKey: true,
+                        }
+                    }
+                }
+            }
+        },
+    });
+
+    if(!data) {
+        return notFound();
+    }
+
+    return data;
+}
+
+export type AdminCoureSingularType = Awaited<ReturnType<typeof adminGetCourse>>;
