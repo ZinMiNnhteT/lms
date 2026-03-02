14cc85d94c1ccd7312628c0770ca8dd76432f850
Zin Min Htet <zinmin337890@gmail.com>
2026-03-02 21:50:47 +0630

    stripe-integration-2026-03-2

--- app/(public)/courses/[slug]/page.tsx
+++ app/(public)/courses/[slug]/page.tsx
@@ -11,6 +11,7 @@ import Image from "next/image";
 import { checkIfCourseBought } from "@/app/data/user/user-is-enrolled";
 import Link from "next/link";
 import { EnrollmentButton } from "./_components/EnrollmentButton";
+import { buttonVariants } from "@/components/ui/button";
 
 type Params = Promise<{ slug: string }>;
 
@@ -236,8 +237,13 @@ export default async function SlugPage({ params }: { params: Params }) {
                                 </ul>
                             </div>
 
-                            {!isEnrolled ? (
-                                <Link href="/dashboard">Watch Course</Link>
+                            {isEnrolled ? (
+                                <Link
+                                    className={buttonVariants({ className: "w-full" })}
+                                    href="/dashboard"
+                                    >
+                                        Watch Course
+                                    </Link>
                             ) : (
                                 <EnrollmentButton courseId={course.id} />
                             )}
