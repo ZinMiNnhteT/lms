8c8465b81a3f344bda96f72b3a09558e7deb05a0
Zin Min Htet <zinmin337890@gmail.com>
2026-03-15 00:34:27 +0630

    fix: reduce middleware size to stay within Vercel limit

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,7 @@
-import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
-import { env } from "./lib/env";
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
-const aj = arcjet({
-  key: env.ARCJET_KEY!,
-  rules: [
-    detectBot({
-      mode: "LIVE",
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", 
-        "CATEGORY:MONITOR",
-        "CATEGORY:PREVIEW",
-        "STRIPE_WEBHOOK",
-      ],
-    }),
-  ],
-});
-
-export async function authMiddleware(request: NextRequest) {
+async function adminAuthHandler(request: NextRequest) {
 	const sessionCookie = getSessionCookie(request);
 
 	if (!sessionCookie) {
@@ -28,13 +11,16 @@ export async function authMiddleware(request: NextRequest) {
 	return NextResponse.next();
 }
 
+export default async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/admin")) {
+    return await adminAuthHandler(request);
+  }
+
+  return NextResponse.next();
+};
+
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
 };
-export default createMiddleware(aj, async (request: NextRequest) => {
-	if (request.nextUrl.pathname.startsWith("/admin")) {
-		return authMiddleware(request);
-	}
-
-	return NextResponse.next();
-});
